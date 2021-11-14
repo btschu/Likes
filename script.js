9fcf33680461d244fa9f53ad21dfe95d25250e23
btschu <92617960+btschu@users.noreply.github.com>
2021-11-13 20:07:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var likeCount1 =9;
+var likeSpan1 = document.querySelector("#count1");
+
+function like1() {
+    likeCount1++;
+    likeSpan1.innerText = likeCount1 + " like(s)";
+}
+
+var likeCount2 =12;
+var likeSpan2 = document.querySelector("#count2");
+
+function like2() {
+    likeCount2++;
+    likeSpan2.innerText = likeCount2 + " like(s)";
+}
+
+var likeCount3 =9;
+var likeSpan3 = document.querySelector("#count3");
+
+function like3() {
+    likeCount3++;
+    likeSpan3.innerText = likeCount3 + " like(s)";
+}
