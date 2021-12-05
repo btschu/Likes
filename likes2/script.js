bf6caebe82203e83a42f2a2c0a6a2c12df628a77
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-12-05 15:58:01 -0700

    simplified js file into 1 function

--- likes2/script.js
+++ likes2/script.js
@@ -1,23 +1,29 @@
-var likeCount1 =9;
-var likeSpan1 = document.querySelector("#count1");
-
-function like1() {
-    likeCount1++;
-    likeSpan1.innerText = likeCount1 + " like(s)";
+function addLike(element) {
+    element.parentElement.querySelector("p>span").innerHTML++
 }
 
-var likeCount2 =12;
-var likeSpan2 = document.querySelector("#count2");
+// original solution
 
-function like2() {
-    likeCount2++;
-    likeSpan2.innerText = likeCount2 + " like(s)";
-}
+// var likeCount1 =9;
+// var likeSpan1 = document.querySelector("#count1");
 
-var likeCount3 =9;
-var likeSpan3 = document.querySelector("#count3");
+// function like1() {
+//     likeCount1++;
+//     likeSpan1.innerText = likeCount1 + " like(s)";
+// }
 
-function like3() {
-    likeCount3++;
-    likeSpan3.innerText = likeCount3 + " like(s)";
-}
+// var likeCount2 =12;
+// var likeSpan2 = document.querySelector("#count2");
+
+// function like2() {
+//     likeCount2++;
+//     likeSpan2.innerText = likeCount2 + " like(s)";
+// }
+
+// var likeCount3 =9;
+// var likeSpan3 = document.querySelector("#count3");
+
+// function like3() {
+//     likeCount3++;
+//     likeSpan3.innerText = likeCount3 + " like(s)";
+// }
